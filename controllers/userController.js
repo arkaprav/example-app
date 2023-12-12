5aa5cf5fd64979f72206cef3152dd613e45731d3
arkaprav <chakraarkaprava@gmail.com>
2023-12-12 23:27:17 +0530

    added user routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,44 @@
+//@desc Gets All Users
+//@route GET /api/users/all
+//@access public
+
 const getAllUser = (req, res) => {
     res.status(200).json({ message: "Get All Users" });
 }
 
-module.exports = { getAllUser };
+//@desc Gets Single User
+//@route GET /api/users/:id
+//@access public
+
+const getSingleUser = (req, res) => {
+    res.status(200).json({ message: `Get User with id: ${req.params.id}` });
+}
+
+//@desc Update Single User
+//@route put /api/users/:id
+//@access public
+
+const updateSingleUser = (req, res) => {
+    res.status(200).json({ message: `Updates User with id: ${req.params.id}` });
+}
+
+
+//@desc Deletes All Users
+//@route DELETE /api/users/:id
+//@access public
+
+const DeleteSingleUser = (req, res) => {
+    res.status(200).json({ message: `Deletes User with id: ${req.params.id}` });
+}
+
+
+//@desc Create User
+//@route POST /api/users/
+//@access public
+
+const createSingleUser = (req, res) => {
+    res.status(200).json({ message: `Creates User` });
+}
+
+
+module.exports = { getAllUser, getSingleUser, updateSingleUser, DeleteSingleUser, createSingleUser };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const { getAllUser } = require("../controllers/userController");
+const { getAllUser, getSingleUser, updateSingleUser, DeleteSingleUser, createSingleUser } = require("../controllers/userController");
 
 const router = express.Router();
 
 router.route("/all").get(getAllUser);
+router.route("/").post(createSingleUser);
+router.route("/:id").get(getSingleUser).put(updateSingleUser).delete(DeleteSingleUser);
 
 module.exports = router;
