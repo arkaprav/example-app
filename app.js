b8cb3f384b22b9b1e30ff04bac0d09684462c604
arkaprav <chakraarkaprava@gmail.com>
2023-12-13 00:06:27 +0530

    added database connection

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const userRoutes = require("./routes/userRoutes");
 const errorHandler = require('./middlewares/errorHandler');
+const ConnectDB = require("./config/dbConfig");
+
+require("dotenv").config();
+
+ConnectDB();
 
 app.use(express.json());
 app.use("/api/users/", userRoutes);
