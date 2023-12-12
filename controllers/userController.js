dec4fadebe76d6ec9ad0735abc7a1107759c6be9
arkaprav <chakraarkaprava@gmail.com>
2023-12-12 23:42:07 +0530

    added error handlers

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,44 +1,51 @@
+const asyncHandler = require("express-async-handler");
+
 //@desc Gets All Users
 //@route GET /api/users/all
 //@access public
 
-const getAllUser = (req, res) => {
-    res.status(200).json({ message: "Get All Users" });
-}
+const getAllUser = asyncHandler( async (req, res) => {
+    await res.status(200).json({ message: "Get All Users" });
+});
 
 //@desc Gets Single User
 //@route GET /api/users/:id
 //@access public
 
-const getSingleUser = (req, res) => {
-    res.status(200).json({ message: `Get User with id: ${req.params.id}` });
-}
+const getSingleUser = asyncHandler( async (req, res) => {
+    await res.status(200).json({ message: `Get User with id: ${req.params.id}` });
+})
 
 //@desc Update Single User
 //@route put /api/users/:id
 //@access public
 
-const updateSingleUser = (req, res) => {
-    res.status(200).json({ message: `Updates User with id: ${req.params.id}` });
-}
+const updateSingleUser = asyncHandler( async (req, res) => {
+    await res.status(200).json({ message: `Updates User with id: ${req.params.id}` });
+});
 
 
 //@desc Deletes All Users
 //@route DELETE /api/users/:id
 //@access public
 
-const DeleteSingleUser = (req, res) => {
-    res.status(200).json({ message: `Deletes User with id: ${req.params.id}` });
-}
+const DeleteSingleUser = asyncHandler( async (req, res) => {
+    await res.status(200).json({ message: `Deletes User with id: ${req.params.id}` });
+});
 
 
 //@desc Create User
 //@route POST /api/users/
 //@access public
 
-const createSingleUser = (req, res) => {
-    res.status(200).json({ message: `Creates User` });
-}
+const createSingleUser = asyncHandler( async (req, res) => {
+    const { name, email, address, phone } = await req.body;
+    if(!name || !email || !address || !phone){
+        res.status(404);
+        throw new Error("All Fields are mandatory");
+    }
+    res.status(200).json({ message: `Creates User with name: ${name}, address: ${address}, email: ${email} and mobile: ${mobile} ` });
+});
 
 
 module.exports = { getAllUser, getSingleUser, updateSingleUser, DeleteSingleUser, createSingleUser };
